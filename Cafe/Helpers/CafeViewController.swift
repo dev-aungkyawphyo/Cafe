93c4adc7ce7fba4c34ec323f5cffe5bbb4effa6d
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-12-18 02:01:08 +0630

    first commit

---
+++ Cafe/Helpers/CafeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CafeViewController.swift
+//  Cafe
+//
+//  Created by Aung Kyaw Phyo on 18/12/2024.
+//
+
+import UIKit
+
+class CafeViewController: UIViewController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+    
+}
